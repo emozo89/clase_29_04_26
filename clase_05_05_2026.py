1301138d454f9162039ff9b243e53cf054886ff0
Emanuel Mozo <e.mozo89gmail.com>
2026-05-05 19:57:02 -0400

    clase_05_05

---
+++ clase_05_05_2026.py
@@ -0,0 +1,33 @@
+import time
+
+#x = 0
+#while x <= 23:
+#   y = 0
+#   while y <= 59:
+#      z = 0
+#      while z <= 59:
+#         print(x, ":", y, ":", z)
+#         time.sleep(0.2)
+#         z = z + 1
+#      y = y + 1
+#   x = x + 1
+#print("Fin")
+
+h = 0
+while h <= 23:
+   m = 0
+   while m <= 59:
+      z = 0
+      while s <= 59:
+         print(h, ":", m, ":", s)
+         time.sleep(0.2)
+         s = s + 1
+      m = m + 1
+   h = h + 1
+print("Fin")
+
+for h in range (24):
+   for m in range (60):
+        for s in range (60):
+            print(h, ":", m, ":", s)
+            time.sleep(0.2)
