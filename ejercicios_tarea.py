8d89808297f817d5a275513e451b9903937a188a
Emanuel Mozo <e.mozo89@gmai.com>
2026-05-08 11:54:26 -0400

    actualización

--- ejercicios_tarea.py
+++ ejercicios_tarea.py
@@ -1,10 +1,3 @@
-#3.-Poblar de forma automatica una lista de tamaño 10 con los
-#1eros numeros primos (divisibles por 1 y por si mismos)
-#la lista debe quedar así: [2,3,5,7,11,13,17,19,23,29]
-#debe ser automatico, porque si pidiera los 1er 100 o 1000
-#debe agregarlos a la lista
-
-
 #1.-Desarrolle un programa que permita ingresar 10 numeros al usuario
 #al finalizar desplegar los elementos y en que posicion de la lista
 #estan los mayores al promedio
@@ -30,5 +23,11 @@ for i in range (len(numeros)):
             print(numeros[i], "esta en posicion", i)
             
             
-#2.-Ordenar de manera ascendente una lista de tamaño 10
-#previamente poblada con numeros (no se puede usar el metodo sort)
+#3.-Poblar de forma automatica una lista de tamaño 10 con los
+#1eros numeros primos (divisibles por 1 y por si mismos)
+#la lista debe quedar así: [2,3,5,7,11,13,17,19,23,29]
+#debe ser automatico, porque si pidiera los 1er 100 o 1000
+#debe agregarlos a la lista
+
+
+
---
+++ ejercicios_tarea2.py
@@ -0,0 +1,2 @@
+#2.-Ordenar de manera ascendente una lista de tamaño 10
+#previamente poblada con numeros (no se puede usar el metodo sort)
