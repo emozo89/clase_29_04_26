fb48502461246ba206f169ed5bb0fd6495b78361
Emanuel Mozo <e.mozo89@gmai.com>
2026-05-08 14:54:40 -0400

    actualización

--- ejercicios_tarea3.py
+++ ejercicios_tarea3.py
@@ -3,3 +3,11 @@
 #la lista debe quedar así: [2,3,5,7,11,13,17,19,23,29]
 #debe ser automatico, porque si pidiera los 1er 100 o 1000
 #debe agregarlos a la lista
+
+lista = []
+i = 0
+
+while i < 10:
+    num = int(input("Ingrese un numero: "))
+    lista.append(num)
+    i = i + 1
