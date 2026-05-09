3bc567ac5d6570dc48e223c10947a39911cd5685
Emanuel Mozo <e.mozo89@gmail.com>
2026-05-08 22:23:46 -0400

    actualizado

---
+++ ejercicio_08_05_26.py
@@ -0,0 +1,20 @@
+
+#def amigos(num1, num2):
+#    suma = 0
+#    for i in range(1, num1):
+#        if num1 % i == 0:
+#            suma = suma + i
+#    if suma == num2:
+#        suma = 0
+#        for i in range(1, num2):
+#            if num2 % i == 0:
+#                suma = suma + i
+#        if suma == num1:
+#           return True
+#    return False
+#print(amigos(220, 284))
+
+
+#1.-Desarrolle una funcion que retorne el factorial de un numero
+
+
